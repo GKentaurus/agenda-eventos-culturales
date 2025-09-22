0ebaedad098f0b97f46943e5a6459962a5d4321f
Carlos Moreno <gkent@outlook.com>
2025-09-22 14:37:32 -0500

    Clean code

--- app/src/main/java/com/app/adec/data/EventsConstants.kt
+++ app/src/main/java/com/app/adec/data/EventsConstants.kt
@@ -14,7 +14,9 @@ class EventsConstants {
             "Loom World Tour",
             "Bogotá, Cundinamarca",
             LocalDateTime.of(2025,10,17,17,0,0),
-            "«The LOOM World Tour» es la quinta gira mundial de conciertos en curso de la banda estadounidense de pop rock Imagine Dragons, creada para apoyar y promocionar el lanzamiento de su sexto álbum de estudio, «LOOM».",
+            "«The LOOM World Tour» es la quinta gira mundial de conciertos en curso " +
+                    "de la banda estadounidense de pop rock Imagine Dragons, creada para apoyar " +
+                    "y promocionar el lanzamiento de su sexto álbum de estudio, «LOOM».",
             R.drawable.logo_0001_imagine_dragons,
             R.drawable.main_0001_imagine_dragons,
             BigDecimal(300000)
@@ -26,7 +28,8 @@ class EventsConstants {
             "Cómo ser carismático sin morir en el intento",
             "Bogotá, Cundinamarca",
             LocalDateTime.now().plusDays(30).minusHours(2),
-            "Aprenda a como captar la atención de las personas y dar a conocer su idea, con herramientas psicosociales demostradas.",
+            "Aprenda a como captar la atención de las personas y dar a conocer su " +
+                    "idea, con herramientas psicosociales demostradas.",
             R.drawable.logo_0002_jorge_duque_linares,
             R.drawable.main_0002_jorge_duque_linares,
             BigDecimal(150000)
@@ -38,7 +41,11 @@ class EventsConstants {
             "From Zero World Tour",
             "Bogotá, Cundinamarca",
             LocalDateTime.of(2025,10,25,19,0,0),
-            "From Zero World Tour es una gira de conciertos en curso de la banda de rock estadounidense Linkin Park, en apoyo de su octavo álbum de estudio From Zero, que fue publicado el 15 de noviembre de 2024. La gira fue anunciada el 5 de septiembre de 2024, tras el lanzamiento del primer sencillo del álbum, «The Emptiness Machine».",
+            "From Zero World Tour es una gira de conciertos en curso de la banda de " +
+                    "rock estadounidense Linkin Park, en apoyo de su octavo álbum de estudio " +
+                    "From Zero, que fue publicado el 15 de noviembre de 2024. La gira fue " +
+                    "anunciada el 5 de septiembre de 2024, tras el lanzamiento del primer " +
+                    "sencillo del álbum, «The Emptiness Machine».",
             R.drawable.logo_0003_linkin_park,
             R.drawable.main_0003_linkin_park,
             BigDecimal(370000)
